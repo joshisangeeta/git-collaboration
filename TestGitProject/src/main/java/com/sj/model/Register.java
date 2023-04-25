0505074c14e61d1023f91d43071a98fef39a52a8
joshisangeeta <sangeeta.joshi2@gmail.com>
2023-04-25 15:41:15 +0530

    fixing register issues

---
+++ TestGitProject/src/main/java/com/sj/model/Register.java
@@ -0,0 +1,25 @@
+package com.sj.model;
+
+public class Register {
+	String regiDetails;
+	
+
+	public Register() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Register(String regiDetails) {
+		super();
+		this.regiDetails = regiDetails;
+	}
+
+	public String getRegiDetails() {
+		return regiDetails;
+	}
+
+	public void setRegiDetails(String regiDetails) {
+		this.regiDetails = regiDetails;
+	}
+   
+}
